3d27709e1da02c5d70cae92ddbb2543c62fe49d9
jdmacmillan3 <118634094+jdmacmillan3@users.noreply.github.com>
2023-09-08 13:58:02 -0400

    Delete yelp.session.sql

--- yelp.session.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM restaurants 
-    LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) AS average_rating 
-    FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id 
-    ORDER BY id;
